b4def81fc8a1e9dc4f08ad7213a32a553c0bf65e
RadomirGross <grossbrn1990@gmail.com>
2024-12-01 21:59:33 +0700

    delete empty imports

--- src/main/java/com/gross/currency_exchange/servlet/ExchangeResultServlet.java
+++ src/main/java/com/gross/currency_exchange/servlet/ExchangeResultServlet.java
@@ -1,12 +1,8 @@
 package com.gross.currency_exchange.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gross.currency_exchange.dao.CurrencyDAOImpl;
-import com.gross.currency_exchange.dao.ExchangeRateDAOImpl;
 import com.gross.currency_exchange.dto.ExchangeResult;
-import com.gross.currency_exchange.mapper.ExchangeRateMapper;
 import com.gross.currency_exchange.service.*;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,18 +15,15 @@ import java.util.NoSuchElementException;
 
 @WebServlet("/exchange")
 public class ExchangeResultServlet extends HttpServlet {
-    private final CurrencyService currencyService = new CurrencyServiceImpl(new CurrencyDAOImpl());
-    private final ExchangeRateService exchangeRateService = new ExchangeRateServiceImpl(new ExchangeRateDAOImpl(), currencyService);
-    private final ExchangeRateMapper exchangeRateMapper = ExchangeRateMapper.INSTANCE;
     private final ExchangeResultService exchangeResultService = new ExchangeResultServiceImpl();
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws  IOException {
 
         String baseCurrencyCode = request.getParameter("from");
         String targetCurrencyCode = request.getParameter("to");
         BigDecimal amount = new BigDecimal(request.getParameter("amount"));
-        ExchangeResult exchangeResult=null;
+        ExchangeResult exchangeResult;
         try {
             exchangeResult = exchangeResultService.
                     getExchangeResult(baseCurrencyCode, targetCurrencyCode, amount);
